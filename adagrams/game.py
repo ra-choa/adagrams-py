822f780294103f1ead2ffebfd0145b582c45f497
Sno <ochoa.sara2036@gmail.com>
2025-03-19 20:43:14 -0700

    passed all tests in test_wave_01

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,31 @@
 from random import randint
 
+LETTER_POOL = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B",
+            "C", "C", "D", "D", "D", "D",
+            "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+            "F", "F", "G", "G", "G", "H", "H",
+            "I", "I", "I", "I", "I", "I", "I", "I", "I",
+            "J", "K", "L", "L", "L", "L",
+            "M", "M", "N", "N", "N", "N", "N", "N",
+            "O", "O", "O", "O", "O", "O", "O", "O",
+            "P", "P", "Q",
+            "R", "R", "R", "R", "R", "R",
+            "S", "S", "S", "S",
+            "T", "T", "T", "T", "T", "T",
+            "U", "U", "U", "U", "V", "V",
+            "W", "W", "X", "Y", "Y", "Z"]
+
+
 def draw_letters():
-    pass
+    tile_hand = []
+    LETTER_POOL_copy = LETTER_POOL.copy()
+    while len(tile_hand) < 10:
+            index = randint(0,len(LETTER_POOL_copy) -1)
+            tile_hand.append(LETTER_POOL_copy[index])
+            LETTER_POOL_copy.remove(LETTER_POOL_copy[index])
+   
+    return tile_hand
+
 
 def uses_available_letters(word, letter_bank):
     pass
